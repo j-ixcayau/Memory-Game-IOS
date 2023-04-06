fdacb9a0e3f49615025976910b9a0592c2b0fcaf
Jonathan Ixcayau <jonynija@gmail.com>
2023-04-05 20:19:56 -0600

    Initial Commit

---
+++ memorygame/memorygameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorygameApp.swift
+//  memorygame
+//
+//  Created by Jontahan Ixcayau on 5/04/23.
+//
+
+import SwiftUI
+
+@main
+struct memorygameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
