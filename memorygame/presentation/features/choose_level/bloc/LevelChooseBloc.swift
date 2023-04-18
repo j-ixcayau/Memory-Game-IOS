23473b078d506c44f8595980735a76cef46dee00
Jonathan Ixcayau <jonynija@gmail.com>
2023-04-18 04:23:22 +0000

    Feature/level choose

---
+++ memorygame/presentation/features/choose_level/bloc/LevelChooseBloc.swift
@@ -0,0 +1,12 @@
+//
+//  LevelChooseBloc.swift
+//  memorygame
+//
+//  Created by Jontahan Ixcayau on 11/04/23.
+//
+
+import Foundation
+
+class LevelChooseBloc: ObservableObject {
+    @Published var difficultyLevel: DifficultyLevel?
+}
